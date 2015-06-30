cc65998901257f5f9f5e487ee4f8fbb3cc6f33a5
nickolanack <nickblackwell82@gmail.com>
2015-06-30 15:56:21 -0700

    add plain index file

--- index.js
+++ index.js
@@ -2,21 +2,25 @@
  * 
  */
 
-function printPage(){
-	
-	return 'hello world';
-	
+function printPage(req){
+
+        return JSON.stringify(Object.keys(req));
+
 }
 
 
 
 
-require('http').createServer(function(req, res) {  
+
+
+
+var http=require('http');
+var server=http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
   res.write(printPage());
   res.end();
-}).listen(80, '127.0.0.1');
+});
 
-
+server.listen(8080);
