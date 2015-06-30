eb345a38dd1db7de3f79c8b251b3ae7e8b356cf4
nickolanack <nickblackwell82@gmail.com>
2015-06-30 16:18:45 -0700

    html folder to make a page with one stylesheet, one js file and and index file

--- index.js
+++ index.js
@@ -5,26 +5,58 @@ var fs=require('fs');
 var http=require('http');
 
 
-function printHtml(req, res){
-	 	res.writeHead(200, {
-		    'Content-Type': 'text/html'
-		  });
-	 	
-		fs.readFile('./index.html', function (err, data) {
-			res.write(data);
-			res.end();
-		});
-		
+function printPage(req, res){
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
 
+	fs.readFile('./html/index.html', function (err, data) {
+		res.write(data);
+		res.end();
+	});
 }
 
 
+function printScript(req, res){
+	res.writeHead(200, {
+		'Content-Type': 'text/javascript'
+	});
+
+	fs.readFile('./html/script.js', function (err, data) {
+		res.write(data);
+		res.end();
+	});
+}
+
+
+function printStylesheet(req, res){
+	res.writeHead(200, {
+		'Content-Type': 'text/css'
+	});
+
+	fs.readFile('./html/style.css', function (err, data) {
+		res.write(data);
+		res.end();
+	});
+}
+
+
+function printRequest(req, res){
+	res.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+
+
+	res.write(req.toString());
+	res.end();
+	
+}
 
 
 var server=http.createServer(function(req, res) {
- 
-	printHtml(req, res);
-  
+
+	printRequest(req, res);
+
 });
 
 server.listen(8080);
