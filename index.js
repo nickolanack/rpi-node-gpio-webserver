edc3c78ce0c239a02bda77067afa870997b5caf4
nickolanack <nickblackwell82@gmail.com>
2015-06-30 16:59:27 -0700

    serve simple site

--- index.js
+++ index.js
@@ -5,57 +5,59 @@ var fs=require('fs');
 var http=require('http');
 
 
-function printPage(req, res){
-	res.writeHead(200, {
-		'Content-Type': 'text/html'
-	});
 
-	fs.readFile('./html/index.html', function (err, data) {
-		res.write(data);
-		res.end();
-	});
-}
-
-
-function printScript(req, res){
-	res.writeHead(200, {
-		'Content-Type': 'text/javascript'
-	});
-
-	fs.readFile('./html/script.js', function (err, data) {
-		res.write(data);
-		res.end();
-	});
-}
-
-
-function printStylesheet(req, res){
-	res.writeHead(200, {
-		'Content-Type': 'text/css'
-	});
-
-	fs.readFile('./html/style.css', function (err, data) {
-		res.write(data);
-		res.end();
-	});
-}
-
-
-function printRequest(req, res){
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-
-
-	res.write(req.toString());
-	res.end();
+function printFile(file, res){
 	
+	fs.readFile('./html/'+file, function (err, data) {
+		res.write(data);
+		res.end();
+	});
 }
+
+
 
 
 var server=http.createServer(function(req, res) {
 
-	printRequest(req, res);
+	var file=req.url.split('/').pop();
+	if(file.indexOf('.')>=0){
+		
+		
+		
+		fs.exists('./html/'+file, function(exists){
+			
+			if(exists){
+				var contentTypes={
+						js:'text/javascript',
+						css:'text/css',
+						html:'text/html',
+							};
+				
+				var type=file.split('.').pop();
+				res.writeHead(200, {
+					'Content-Type': contentTypes[type]
+				});
+				printFile(file, res);
+			
+			}else{
+				
+				res.writeHead(404);
+				res.end('File not found: '+file);
+				
+			}
+			
+			
+			
+		});
+	}else{
+		
+		res.writeHead(500);
+		res.end('resuest: '+file);
+		
+	}
+	
+	
+
 
 });
 
