824333085752af14d63b93826b8a009692415706
nickola <nickblackwell82@gmail.com>
2015-07-01 19:37:04 +0000

    added websocket server

--- index.js
+++ index.js
@@ -1,62 +1,103 @@
 /**
  * 
  */
-var fs=require('fs');
-var http=require('http');
-var async=require('async');
-var port=80;
-var documentRoot='./html/';
 
-var server=http.createServer(function(req, res) {
+(function(){
+	
+	//Simple webserver
 
-	var file=req.url.split('/').pop();
-	if(file===''){
-		file='index.html';
-	}
-	if(file.indexOf('.')>=0){
+	var fs=require('fs');
+	var http=require('http');
+	var async=require('async');
+	var port=80;
+	var documentRoot='./html/';
 
+	var server=http.createServer(function(req, res) {
+
+		var file=req.url.split('/').pop();
+		if(file===''){
+			file='index.html';
+		}
+		if(file.indexOf('.')>=0){
+	
+			fs.exists(documentRoot+file, function(exists){
 			
-		fs.exists(documentRoot+file, function(exists){
-			
-			if(exists){
-				var contentTypes={
+				if(exists){
+					var contentTypes={
 						js:'text/javascript',
 						css:'text/css',
-						html:'text/html',
-							};
-				
-				var type=file.split('.').pop();
-				res.writeHead(200, {
-					'Content-Type': contentTypes[type]
-				});
-
-				fs.readFile(documentRoot+file, function (err, data) {
-					res.write(data);
-					res.end();
-				});
-
-				
+						html:'text/html'
+					};
+					
+					var type=file.split('.').pop();
+					res.writeHead(200, {
+						'Content-Type': contentTypes[type]
+					});
+		
+					fs.readFile(documentRoot+file, function (err, data) {
+						res.write(data);
+						res.end();
+					});
 			
-			
-			}else{
-				
-				res.writeHead(404);
-				res.end('File not found: '+file);
-				
-			}
+				}else{
+						
+					res.writeHead(404);
+					res.end('File not found: '+file);
+					
+				}
 
+			});
+		}else{
+			
+			res.writeHead(500);
+			res.end('resuest: '+file);
+			
+		}
+		
+	});
+	
+	server.listen(port);
+	console.log('webserver listening on: '+port);
+
+})();
+
+(function(){
+
+	// Simple websocket server
+	
+	var port = 8080;
+	var clients = [];
+
+	(new (require('ws').Server)({
+		port: port
+	})).on('connection', function(wsclient){
+	
+		clients.push(wsclient);
+
+		wsclient.on('message',function(data){
+
+			console.log(data);
+		
+		}).on('close',function(code, message){
+			var i = clients.indexOf(wsclient);
+			console.log('ws:'+i+' client closed: '+code+' '+message);
+			clients.splice(i, 1);
 		});
-	}else{
+
+	}).on('headers', function(headers){
 		
-		res.writeHead(500);
-		res.end('resuest: '+file);
+		console.log('headers: '+headers);
+	
+	}).on('error', function(error){
 		
-	}
+		console.log('error: '+error);
 	
-	
+	});
+
+	console.log('websocket listening on: '+port);
+
+})();
 
 
-});
 
-server.listen(port);
-console.log('listening on: '+port);
+
