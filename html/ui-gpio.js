6bc324fd643bca95ad6b1e9ff22e5fe879e992fd
nickolanack <nickblackwell82@gmail.com>
2015-07-30 21:59:49 -0700

    add a delay for testing

--- html/ui-gpio.js
+++ html/ui-gpio.js
@@ -7,7 +7,7 @@ var UIGeneralPurposeIOPanel=new Class({
 
 
 	initialize:function(element, options){
-		
+
 		var me=this;
 		me.options=Object.append({
 			websocket:'ws://'+window.location.hostname+':8080'
@@ -32,54 +32,56 @@ var UIGeneralPurposeIOPanel=new Class({
 			}
 
 
+			setTimeout(function(){
 
+				getDevices(function(devices){
 
-			getDevices(function(devices){
+					websocket.addEvent('notification.statechange', function(response){
 
-				websocket.addEvent('notification.statechange', function(response){
+						var data=JSON.parse(response);
+						Array.each(devices,function(device){
 
-					var data=JSON.parse(response);
-					Array.each(devices,function(device){
+							if(device.pin===data.pin){
 
-						if(device.pin===data.pin){
-
-							// control is added to device on initialization below.	
-							device._suppressEventSignal=true;
-							device.control.setValue(data.value);	
-							// TODO: the event for this setValue task, should be suppressed, otherwise
-							// a notification will be sent to the server, and strange race conditions 
-							// could occur.
-						}
-
-					});
-
-				});
-
-
-				Array.each(devices,function(device){
-					var state=device.state;
-					var container=element.appendChild(new Element('div'));
-					var control=new UISwitchControl(container, {state:state}).addEvent("change",function(newState){
-						if(state!=newState){
-							if(!device._suppressEventSignal){
-								signalDeviceValue(device.pin, newState);
-							}else{
-								delete device._suppressEventSignal;
+								// control is added to device on initialization below.	
+								device._suppressEventSignal=true;
+								device.control.setValue(data.value);	
+								// TODO: the event for this setValue task, should be suppressed, otherwise
+								// a notification will be sent to the server, and strange race conditions 
+								// could occur.
 							}
 
-							state=newState;
-						}
-					});
-					container.appendChild(new Element('label', {html:device.name}));
-					if(device.direction==='in'){
-						control.disable(); //read display only device
-					}
+						});
 
-					device.control=control; 
+					});
+
+
+					Array.each(devices,function(device){
+						var state=device.state;
+						var container=element.appendChild(new Element('div'));
+						var control=new UISwitchControl(container, {state:state}).addEvent("change",function(newState){
+							if(state!=newState){
+								if(!device._suppressEventSignal){
+									signalDeviceValue(device.pin, newState);
+								}else{
+									delete device._suppressEventSignal;
+								}
+
+								state=newState;
+							}
+						});
+						container.appendChild(new Element('label', {html:device.name}));
+						if(device.direction==='in'){
+							control.disable(); //read display only device
+						}
+
+						device.control=control; 
+
+					});
 
 				});
 
-			});
+			},1000);
 
 		});
 
