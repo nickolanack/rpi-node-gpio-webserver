c92ecf082a3f3fd21fc748a5b5a4594c01b9f61d
nickolanack <nickblackwell82@gmail.com>
2015-07-30 20:56:50 -0700

    can disable the webserver or the socketserver by setting the port in server.json to false useful splitting the application into seperate threads. and creating proxy mirrors

--- index.js
+++ index.js
@@ -3,90 +3,92 @@
  */
 
 var config=require('./server.json');
-var devices=require('./devices.json');
+if(config.serverPort!==false){
 
-(function(){
-	
-	//Simple webserver
+	(function(){
 
-	var fs=require('fs');
-	var http=require('http');
-	var async=require('async');
-	var port=config.serverPort;
-	var documentRoot='./html/';
+		//Simple webserver
 
-	var server=http.createServer(function(req, res) {
+		var fs=require('fs');
+		var http=require('http');
+		//var async=require('async');
+		var port=config.serverPort;
+		var documentRoot='./html/';
 
-		var file=req.url.split('/').pop();
-		if(file===''){
-			file='index.html';
-		}
-		if(file.indexOf('.')>=0){
-	
-			fs.exists(documentRoot+file, function(exists){
-			
-				if(exists){
-					var contentTypes={
-						js:'text/javascript',
-						css:'text/css',
-						html:'text/html'
-					};
-					
-					var type=file.split('.').pop();
-					res.writeHead(200, {
-						'Content-Type': contentTypes[type]
-					});
-		
-					fs.readFile(documentRoot+file, function (err, data) {
-						res.write(data);
-						res.end();
-					});
-			
-				}else{
-						
-					res.writeHead(404);
-					res.end('File not found: '+file);
-					
-				}
+		var server=http.createServer(function(req, res) {
 
+			var file=req.url.split('/').pop();
+			if(file===''){
+				file='index.html';
+			}
+			if(file.indexOf('.')>=0){
+
+				fs.exists(documentRoot+file, function(exists){
+
+					if(exists){
+						var contentTypes={
+								js:'text/javascript',
+								css:'text/css',
+								html:'text/html'
+						};
+
+						var type=file.split('.').pop();
+						res.writeHead(200, {
+							'Content-Type': contentTypes[type]
+						});
+
+						fs.readFile(documentRoot+file, function (err, data) {
+							res.write(data);
+							res.end();
+						});
+
+					}else{
+
+						res.writeHead(404);
+						res.end('File not found: '+file);
+
+					}
+
+				});
+			}else{
+
+				res.writeHead(500);
+				res.end('resuest: '+file);
+
+			}
+
+		});
+
+		server.listen(port);
+		console.log('webserver listening on: '+port);
+
+	})();
+}
+
+if(config.websocketPort!==false){
+
+	var devices=require('./devices.json');
+
+	var gpio = require('rpi-gpio');
+	devices.forEach(function(device){
+
+		var direction=device.direction==='in'?gpio.DIR_IN:gpio.DIR_OUT;
+
+		gpio.setup(device.pin, direction, function(err){
+
+			gpio.read(device.pin, function(err, value) {
+				console.log('device: '+device.pin+' initial state: ' + value);
+				device.state=value?true:false;
 			});
-		}else{
-			
-			res.writeHead(500);
-			res.end('resuest: '+file);
-			
-		}
-		
+
+		});
+
+
+
 	});
-	
-	server.listen(port);
-	console.log('webserver listening on: '+port);
 
-})();
-
-
-
-	
-var gpio = require('rpi-gpio');
-devices.forEach(function(device){
-	
-	var direction=device.direction==='in'?gpio.DIR_IN:gpio.DIR_OUT;
-	
-	gpio.setup(device.pin, direction, function(err){
-		
-		 gpio.read(device.pin, function(err, value) {
-		        console.log('device: '+device.pin+' initial state: ' + value);
-		        device.state=value?true:false;
-		 });
-		 
-	});
-	 
-	
-	
-});
-
-/* detect changes. another application could set gpio pins simultaneously.
- * 
+	/* detect changes. another application could set gpio pins simultaneously.
+	 * 
 gpio.on('change', function(pin, value) {
     devices.forEach(function(device){
     	if(device.pin===pin){
@@ -94,110 +96,110 @@ gpio.on('change', function(pin, value) {
     	}
     });
 });
-*/
-	
-	
-var setDeviceState=function(pin, value, callback){
-	
-    gpio.write(pin, value, function(err) {
-        if (err) throw err;
-        
-        
-        devices.forEach(function(device){
-        	if(device.pin===pin){
-        		device.state=value;
-        	}
-        });  
-        callback(value);
-    });
-	
-	
-}
-var clientCanSetPin=function(client, pin){
-	return isOutputPin(pin);
-};
+	 */
 
-var isOutputPin=function(pin){
-	return true;
-};
 
-(function(){
+	var setDeviceState=function(pin, value, callback){
 
-	// Simple websocket server
-	
-	var port = config.websocketPort;
-	var clients = [];
+		gpio.write(pin, value, function(err) {
+			if (err) throw err;
 
-	(new (require('ws').Server)({
-		port: port
-	})).on('connection', function(wsclient){
-	
-		clients.push(wsclient);
-		console.log('client connected: '+wsclient);
 
-		wsclient.on('message',function(data){
-
-		
-			
-			
-			var request=JSON.parse(data);
-			//console.log([data, request]);
-			var id=request.id;
-			var task=request.task;
-			var arguments=request.json;
-			
-			
-			if(task=='list_devices'){
-				wsclient.send(id+':'+JSON.stringify(devices));
-				console.log('sent device list: '+devices.length+' devices');
-
-			}
-			
-			
-			if(task=='set_device_value'){
-				var pin=arguments.pin;
-				var value=!!arguments.value;
-				if(clientCanSetPin(wsclient, pin)){
-					setDeviceState(pin, value, function(value){
-						wsclient.send(id+':set '+pin+' to '+ value);
-						console.log('set device: '+pin+' to '+ value);
-						
-						
-						
-						clients.forEach(function(otherclient){
-							if(otherclient!==wsclient){
-								otherclient.send('notification.statechange:'+JSON.stringify({pin:pin, value:value}));
-							}
-					    });
-					});
+			devices.forEach(function(device){
+				if(device.pin===pin){
+					device.state=value;
 				}
-				
-			}
-			
-		
-		}).on('close',function(code, message){
-			var i = clients.indexOf(wsclient);
-			console.log('ws:'+i+' client closed: '+code+' '+message);
-			clients.splice(i, 1);
+			});  
+			callback(value);
 		});
 
-	}).on('error', function(error){
-		
-		console.log('error: '+error);
-	
-	});
-	
-	
-	//gpio.on('change', function(pin, value) {
-	//   console.log('notify device: '+pin+' state change: '+value);
-	//    
-	//});
 
-	
-	console.log('websocket listening on: '+port);
+	}
+	var clientCanSetPin=function(client, pin){
+		return isOutputPin(pin);
+	};
 
-})();
+	var isOutputPin=function(pin){
+		return true;
+	};
+
+	(function(){
+
+		// Simple websocket server
+
+		var port = config.websocketPort;
+		var clients = [];
+
+		(new (require('ws').Server)({
+			port: port
+		})).on('connection', function(wsclient){
+
+			clients.push(wsclient);
+			console.log('client connected: '+wsclient);
+
+			wsclient.on('message',function(data){
 
 
+
+
+				var request=JSON.parse(data);
+				//console.log([data, request]);
+				var id=request.id;
+				var task=request.task;
+				var arguments=request.json;
+
+
+				if(task=='list_devices'){
+					wsclient.send(id+':'+JSON.stringify(devices));
+					console.log('sent device list: '+devices.length+' devices');
+
+				}
+
+
+				if(task=='set_device_value'){
+					var pin=arguments.pin;
+					var value=!!arguments.value;
+					if(clientCanSetPin(wsclient, pin)){
+						setDeviceState(pin, value, function(value){
+							wsclient.send(id+':set '+pin+' to '+ value);
+							console.log('set device: '+pin+' to '+ value);
+
+
+
+							clients.forEach(function(otherclient){
+								if(otherclient!==wsclient){
+									otherclient.send('notification.statechange:'+JSON.stringify({pin:pin, value:value}));
+								}
+							});
+						});
+					}
+
+				}
+
+
+			}).on('close',function(code, message){
+				var i = clients.indexOf(wsclient);
+				console.log('ws:'+i+' client closed: '+code+' '+message);
+				clients.splice(i, 1);
+			});
+
+		}).on('error', function(error){
+
+			console.log('error: '+error);
+
+		});
+
+
+		//gpio.on('change', function(pin, value) {
+		//   console.log('notify device: '+pin+' state change: '+value);
+		//    
+		//});
+
+
+		console.log('websocket listening on: '+port);
+
+	})();
+
+}
 
 
