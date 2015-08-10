498e8874272b2b92c586cc8ada737462aab37639
nickolanack <nickblackwell82@gmail.com>
2015-08-09 20:57:37 -0700

    use tinywebjs

--- index.js
+++ index.js
@@ -7,60 +7,10 @@ if(config.serverPort!==false){
 
 	(function(){
 
-		//Simple webserver
-
-		var fs=require('fs');
-		var http=require('http');
-		//var async=require('async');
-		var port=config.serverPort;
-		var documentRoot='./html/';
-
-		var server=http.createServer(function(req, res) {
-
-			var file=req.url.split('/').pop();
-			if(file===''){
-				file='index.html';
-			}
-			if(file.indexOf('.')>=0){
-
-				fs.exists(documentRoot+file, function(exists){
-
-					if(exists){
-						var contentTypes={
-								js:'text/javascript',
-								css:'text/css',
-								html:'text/html'
-						};
-
-						var type=file.split('.').pop();
-						res.writeHead(200, {
-							'Content-Type': contentTypes[type]
-						});
-
-						fs.readFile(documentRoot+file, function (err, data) {
-							res.write(data);
-							res.end();
-						});
-
-					}else{
-
-						res.writeHead(404);
-						res.end('File not found: '+file);
-
-					}
-
-				});
-			}else{
-
-				res.writeHead(500);
-				res.end('resuest: '+file);
-
-			}
-
-		});
-
-		server.listen(port);
-		console.log('webserver listening on: '+port);
+		require('tinywebjs').Server({
+			port:config.serverPort,
+			documentRoot:__dirname+'/html/'
+		})
 
 	})();
 }
