0d2ba60b80a7f43368e00c5b5fea873d184f0689
nickolanack <nickblackwell82@gmail.com>
2015-08-09 22:24:57 -0700

    something weird

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ gpio.on('change', function(pin, value) {
 					console.log('set device: '+pin+' to '+ value);
 
 					wsserver.broadcast('notification.statechange', JSON.stringify({pin:pin, value:value}),function(wsclient){
-						return wsclient!==arguments.client;
+						return wsclient!==arguments.client?true:false;
 					});
 					
 				});
