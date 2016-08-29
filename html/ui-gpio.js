abc87a31abbb00a1f3169927a65aee2425172802
nickolanack <nickblackwell82@gmail.com>
2016-08-28 21:51:58 -0700

    updated ws.js (websocket class) to attempt to reconnect if it becomes disconnected) updated ui-gpio.js (gpio ui) to update list after reconnect, and enable/disable controls during/after interruption

--- html/ui-gpio.js
+++ html/ui-gpio.js
@@ -2,41 +2,44 @@
  * 
  */
 
-var UIGeneralPurposeIOPanel=new Class({
-	Implements:Events,
+var UIGeneralPurposeIOPanel = new Class({
+	Implements: Events,
 
-	initialize:function(element, options){
+	initialize: function(element, options) {
 
-		var me=this;
-		me.options=Object.append({
-			websocket:'ws://'+window.location.hostname+':8080'
-		},options);
+		var me = this;
+		me.options = Object.append({
+			websocket: 'ws://' + window.location.hostname + ':8080'
+		}, options);
 
-		var websocket=new WebsocketControlQuery(me.options.websocket).addEvent('connect',function(){
+		var websocket = new WebsocketControlQuery(me.options.websocket).addEvent('connect', function() {
 
-			var getDevices=function(callback){
-				websocket.execute('list_devices', {}, function(response){
+			var getDevices = function(callback) {
+				websocket.execute('list_devices', {}, function(response) {
 					callback(JSON.parse(response));
 				});
 			};
 
-			var signalDeviceValue=function(pin, value){
-				websocket.execute('set_device_value', {pin:pin, value:value}, function(response){
+			var signalDeviceValue = function(pin, value) {
+				websocket.execute('set_device_value', {
+					pin: pin,
+					value: value
+				}, function(response) {
 					console.log(response);
 				});
 			}
 
-			getDevices(function(devices){
+			getDevices(function(devices) {
 
-				websocket.addEvent('notification.statechange', function(response){
+				websocket.addEvent('notification.statechange', function(response) {
 
-					var data=JSON.parse(response);
-					Array.each(devices,function(device){
+					var data = JSON.parse(response);
+					Array.each(devices, function(device) {
 
-						if(device.pin===data.pin){
+						if (device.pin === data.pin) {
 
-							device._suppressEventSignal=true;
-							device.control.setValue(data.value);	
+							device._suppressEventSignal = true;
+							device.control.setValue(data.value);
 
 						}
 
@@ -44,30 +47,69 @@ var UIGeneralPurposeIOPanel=new Class({
 
 				});
 
-				Array.each(devices,function(device){
-					
-					var state=device.state;
-					var container=element.appendChild(new Element('div'));
-					var control=new UISwitchControl(container, {state:state}).addEvent("change",function(newState){
-						if(state!=newState){
-							if(!device._suppressEventSignal){
+				Array.each(devices, function(device) {
+
+					var state = device.state;
+					var container = element.appendChild(new Element('div'));
+					var control = new UISwitchControl(container, {
+						state: state
+					}).addEvent("change", function(newState) {
+						if (state != newState) {
+							if (!device._suppressEventSignal) {
 								signalDeviceValue(device.pin, newState);
-							}else{
+							} else {
 								// state was set on a notification from server. 
 								// it is not necessary to re-signal the state back 
 								// to the server and could cause an infinite loop 
 								delete device._suppressEventSignal;
 							}
 
-							state=newState;
+							state = newState;
 						}
 					});
-					container.appendChild(new Element('label', {html:device.name}));
-					
-					if(device.direction==='in'){
+					container.appendChild(new Element('label', {
+						html: device.name
+					}));
+
+					if (device.direction === 'in') {
 						control.disable(); //read display only device
 					}
-					device.control=control; 
+					device.control = control;
+
+				});
+
+				websocket.addEvent('reconnect', function() {
+
+					//update all devices with current state from request
+
+					getDevices(function(updatedDevices) {
+						Array.each(updatedDevices, function(updatedDevice) {
+							var state = updatedDevice.state;
+							Array.each(devices, function(device) {
+								if (device.pin === updatedDevice.pin) {
+									device._suppressEventSignal = true;
+									device.control.setValue(state);
+								}
+
+							});
+
+						});
+					});
+
+				});
+
+				websocket.addEvent('disconnect', function() {
+					//disable switches while disconnected
+					Array.each(devices, function(device) {
+						device.control.disable();
+					});
+
+				});
+				websocket.addEvent('reconnect', function() {
+					//enable switches when reconnected
+					Array.each(devices, function(device) {
+						device.control.enable();
+					});
 
 				});
 
